a88ffe5651eddb617795c7727ca434ce0491465f
neko-pro <117796661+neko-pro@users.noreply.github.com>
2025-05-03 13:52:21 +0700

    Update fastbull.ts

--- server/sources/fastbull.ts
+++ server/sources/fastbull.ts
@@ -3,7 +3,7 @@ import type { NewsItem } from "@shared/types"
 
 const express = defineSource(async () => {
   const baseURL = "https://www.fastbull.com"
-  const html: any = await myFetch(`${baseURL}/cn/express-news`)
+  const html: any = await myFetch(`${baseURL}/id/express-news`)
   const $ = cheerio.load(html)
   const $main = $(".news-list")
   const news: NewsItem[] = []
@@ -27,7 +27,7 @@ const express = defineSource(async () => {
 
 const news = defineSource(async () => {
   const baseURL = "https://www.fastbull.com"
-  const html: any = await myFetch(`${baseURL}/cn/news`)
+  const html: any = await myFetch(`${baseURL}/id/news`)
   const $ = cheerio.load(html)
   const $main = $(".trending_type")
   const news: NewsItem[] = []
