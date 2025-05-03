b570c00db56715fa538b3779ad77a00cf84ebfa2
neko-pro <117796661+neko-pro@users.noreply.github.com>
2025-05-03 14:00:29 +0700

    Update card.tsx

--- src/components/column/card.tsx
+++ src/components/column/card.tsx
@@ -172,9 +172,9 @@ function NewsCard({ id, setHandleRef }: NewsCardProps) {
 
 function UpdatedTime({ isError, updatedTime }: { updatedTime: any, isError: boolean }) {
   const relativeTime = useRelativeTime(updatedTime ?? "")
-  if (relativeTime) return `${relativeTime}更新`
-  if (isError) return "获取失败"
-  return "加载中..."
+  if (relativeTime) return `${relativeTime}update`
+  if (isError) return "Acquisition failed"
+  return "Loading..."
 }
 
 function DiffNumber({ diff }: { diff: number }) {
