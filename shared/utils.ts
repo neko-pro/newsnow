90d5318525073ee819d6b7a007dc8dbebc20cc8e
neko-pro <117796661+neko-pro@users.noreply.github.com>
2025-05-03 13:48:51 +0700

    Update utils.ts

--- shared/utils.ts
+++ shared/utils.ts
@@ -9,17 +9,17 @@ export function relativeTime(timestamp: string | number) {
   const diffInHours = diffInMinutes / 60
 
   if (diffInSeconds < 60) {
-    return "刚刚"
+    return "Just now"
   } else if (diffInMinutes < 60) {
     const minutes = Math.floor(diffInMinutes)
-    return `${minutes}分钟前`
+    return `${minutes}Minutes ago`
   } else if (diffInHours < 24) {
     const hours = Math.floor(diffInHours)
-    return `${hours}小时前`
+    return `${hours}Hours ago`
   } else {
     const month = date.getMonth() + 1
     const day = date.getDate()
-    return `${month}月${day}日`
+    return `${month}month${day}day`
   }
 }
 
